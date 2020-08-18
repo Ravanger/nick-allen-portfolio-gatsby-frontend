05e25b212afacb894cb5e0292818223410f1abd3
Ravanger <Boris.Rozzy@gmail.com>
2020-08-17 20:05:12 -0400

    Header border

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,6 +7,8 @@ import Contact_Mini from '@components/Contact_Mini'
 const HeaderWrapper = styled.header`
   display: flex;
   align-items: center;
+  border-bottom: solid 2px #f39836;
+  padding-bottom: 1rem;
 `
 
 const Header: React.FC = () => {

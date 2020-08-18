34a734d9377ece41988faccee67e3017abc0fc5a
Ravanger <Boris.Rozzy@gmail.com>
2020-08-17 22:10:49 -0400

    Move global CSS to gatsby-browser

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,3 @@
 import './src/static/style/reset.css'
 import './src/static/fonts/fonts.css'
+import './src/static/style/global.css'
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,22 +1,7 @@
 import React from 'react'
-import { Global, css } from '@emotion/core'
 import styled from '@emotion/styled'
 
 import Header from '@components/Header'
-
-const globalStyles = css`
-  body {
-    background-color: #fff2e4;
-    font-family: Roboto, sans-serif;
-    color: #2f373e;
-    overflow: hidden;
-  }
-
-  a {
-    color: #2f373e;
-    text-decoration: none;
-  }
-`
 
 const MainContent = styled.main`
   padding: 2rem;
@@ -29,7 +14,6 @@ type PropTypes = {
 const Layout: React.FC<PropTypes> = ({ children }) => {
   return (
     <>
-      <Global styles={globalStyles} />
       <Header />
       <MainContent>{children}</MainContent>
     </>

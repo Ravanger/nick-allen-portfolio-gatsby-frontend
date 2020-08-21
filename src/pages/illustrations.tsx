3ac96f1c0c9752dc36ae5ca450a105596f3655b7
Ravanger <Boris.Rozzy@gmail.com>
2020-08-21 15:25:11 -0400

    Illustrations page masonry layout

--- src/pages/illustrations.tsx
+++ src/pages/illustrations.tsx
@@ -1,21 +1,53 @@
 import React from 'react'
 import styled from '@emotion/styled'
+import Masonry from 'react-masonry-css'
 
 import Layout from '@components/Layout'
 import SEO from '@components/SEO'
 
 const DivGallery = styled.div`
-  display: grid;
-  justify-items: center;
-  align-items: center;
-  height: 100vh;
+  max-width: 52rem;
+  margin: 0 auto;
+  padding-top: 7rem;
+  text-align: center;
+
+  > div {
+    display: flex;
+    width: auto;
+
+    > div {
+      padding-left: 0.5em;
+      padding-right: 0.5em;
+      > p {
+        background: grey;
+        margin-bottom: 30px;
+        padding: 1em;
+      }
+    }
+  }
 `
 
 const IllustrationsPage: React.FC = () => {
   return (
     <Layout>
       <SEO title="Illustrations" />
-      <DivGallery>Illustrations. Under construction.</DivGallery>
+      <DivGallery>
+        <Masonry
+          breakpointCols={{
+            default: 3,
+            900: 2,
+            600: 1,
+          }}
+          className=""
+          columnClassName=""
+        >
+          <p>Illustrations. Under construction.</p>
+          <p>Illustrations. Under construction.</p>
+          <p>Illustrations. Under construction.</p>
+          <p>Illustrations. Under construction.</p>
+          <p>Illustrations. Under construction.</p>
+        </Masonry>
+      </DivGallery>
     </Layout>
   )
 }

0f559b61f2215f46088d9c753c4ff5214859d93d
Ravanger <Boris.Rozzy@gmail.com>
2020-08-22 22:14:14 -0400

    Socials links from Strapi

--- src/components/Socials_Icons.tsx
+++ src/components/Socials_Icons.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from '@emotion/styled'
 import { FaTwitter, FaInstagram, FaDribbble, FaBehance } from 'react-icons/fa'
-import { Link } from 'gatsby'
+import { useStaticQuery, graphql } from 'gatsby'
 
 const UlSocialsIcons = styled.ul`
   display: flex;
@@ -14,29 +14,48 @@ type SocialsIconsTypes = {
 }
 
 const Socials_Icons: React.FC<SocialsIconsTypes> = props => {
+  const { strapiSocial } = useStaticQuery(graphql`
+    query {
+      strapiSocial {
+        Behance
+        Dribbble
+        Instagram
+        Twitter
+      }
+    }
+  `)
+
   return (
     <nav>
       <UlSocialsIcons>
-        <li>
-          <Link to="/">
-            <FaTwitter fontSize={props.fontsize} />
-          </Link>
-        </li>
-        <li>
-          <Link to="/">
-            <FaInstagram fontSize={props.fontsize} />
-          </Link>
-        </li>
-        <li>
-          <Link to="/">
-            <FaDribbble fontSize={props.fontsize} />
-          </Link>
-        </li>
-        <li>
-          <Link to="/">
-            <FaBehance fontSize={props.fontsize} />
-          </Link>
-        </li>
+        {strapiSocial.Twitter && strapiSocial.Twitter.length > 0 && (
+          <li>
+            <a href={strapiSocial.Twitter} target="_blank">
+              <FaTwitter fontSize={props.fontsize} />
+            </a>
+          </li>
+        )}
+        {strapiSocial.Instagram && strapiSocial.Instagram.length > 0 && (
+          <li>
+            <a href={strapiSocial.Instagram} target="_blank">
+              <FaInstagram fontSize={props.fontsize} />
+            </a>
+          </li>
+        )}
+        {strapiSocial.Dribbble && strapiSocial.Dribbble.length > 0 && (
+          <li>
+            <a href={strapiSocial.Dribbble} target="_blank">
+              <FaDribbble fontSize={props.fontsize} />
+            </a>
+          </li>
+        )}
+        {strapiSocial.Behance && strapiSocial.Behance.length > 0 && (
+          <li>
+            <a href={strapiSocial.Behance} target="_blank">
+              <FaBehance fontSize={props.fontsize} />
+            </a>
+          </li>
+        )}
       </UlSocialsIcons>
     </nav>
   )
